172dd5725c1db713962f00795ca0b0794b95b9c7
keqae <keqither@gmail.com>
2022-08-20 08:00:48 +0100

    -Added 7 new entities for raiding, added randomizer for raiders

--- src/modules/statue.py
+++ src/modules/statue.py
@@ -1,4 +1,6 @@
 from src.objects import items
+from src.rng import rng
+from src.objects import entities
 
 name = "statue"
 aliases = ["offer", "offering"]
@@ -11,6 +13,7 @@ offerable_items = []
 for item in items.all_items:
     if item.magic_power > 0:
         offerable_items.append(item)
+
 
 def run(p, args):
     obj_s_i = []
@@ -30,7 +33,7 @@ def run(p, args):
 
     offering = int(input(f"\nchoose an item (1-{len(l_string)}) >> "))
 
-    item = obj_s_i[offering-1]
+    item = obj_s_i[offering - 1]
 
     trade = p.trade(
         loses=[item],
@@ -41,20 +44,46 @@ def run(p, args):
     else:
         print("oof, looks like you don't have this item")
 
+
+list_weak_raiders = [
+    entities.beast,
+    entities.hound,
+    entities.goblin
+]
+select_weak_raiders = rng.Roulette(
+    choices=list_weak_raiders,
+    nothing_chance=0
+)
+list_strong_raiders = [
+    entities.bull,
+    entities.boa,
+    entities.mutant_bee
+]
+select_strong_raiders = rng.Roulette(
+    choices=list_strong_raiders,
+    nothing_chance=0
+)
+list_invasion_raiders = [
+    entities.raider
+]
+select_invasion_raiders = rng.Roulette(
+    choices=list_invasion_raiders,
+    nothing_chance=0
+)
+
+
 def battle():
     def weak_raid():
-        for i in obj_s_i[i-1]:
+        for i in obj_s_i[i - 1]:
             if item.magic_power >= 1:
                 print("SUDDENLY,  you see {} charging towards you")
+
     def strong_raid():
-        for i in obj_s_i[i-1]:
+        for i in obj_s_i[i - 1]:
             if item.magic_power > 100:
                 print("SUDDENLY, you see {} charging towards you")
 
     def invasion():
-        for i in obj_s_i[i-1]:
+        for i in obj_s_i[i - 1]:
             if item.magic_power > 200:
                 print("SUDDENLY, you see {} rushing towards you")
-
-
-
--- src/objects/entities.py
+++ src/objects/entities.py
@@ -5,8 +5,8 @@ from src.classes.entity import Entity
 fire_dragon = Entity(
     name="FIRE-BREATHING DRAGON",
     description=f"A big scary fire-breathing dragon, you should probably run.",
-    hp=50,
-    damage=35,
+    hp=random.randint(50, 60),
+    damage=random.randint(30, 40),
     hostile=True,
     rarity=1
 )
@@ -14,8 +14,8 @@ fire_dragon = Entity(
 ogre = Entity(
     name="OGRE",
     description="A goofy looking ogre, doesn't look that dangerous right..?",
-    hp=random.randint(20,30),
-    damage=random.randint(8,12),
+    hp=random.randint(20, 30),
+    damage=random.randint(8, 12),
     hostile=True,
     rarity=12
 )
@@ -33,8 +33,8 @@ zombie = Entity(
     name="Zombie",
     hostile=True,
     description="A regular zombie rotting away in the sun",
-    hp=random.randint(7,13),
-    damage=random.randint(2,4),
+    hp=random.randint(7, 13),
+    damage=random.randint(2, 4),
     rarity=14
 )
 
@@ -69,26 +69,76 @@ pig = Entity(
 
 # region raiders
 
+# weak raids
+
 beast = Entity(
     name="Hungry beast",
     hostile=True,
     description="Stay near and you're lunch",
-    hp=random.randint(20,23),
-    damage=random.randint(9,11),
-    agility=random.randint(3,5),
-    rarity=50
+    hp=random.randint(13, 17),
+    damage=random.randint(9, 11),
+    agility=random.randint(3, 5),
+    rarity=2
 )
 
 hound = Entity(
     name="Bloodhound",
     hostile=True,
     description="Bloodshot eyes, how did they even get that?",
-    hp=random.randint(10,13),
-    damage=random.randint(9,11),
-    agility=random.randint(15,20),
-    rarity=50
+    hp=random.randint(10, 13),
+    damage=random.randint(9, 11),
+    agility=random.randint(15, 20),
+    rarity=2
 )
 
-# jus text me on discord man
+goblin = Entity(
+    name="Goblin",
+    hostile=True,
+    description="Quickkk, hide your gold!",
+    hp=random.randint(16, 20),
+    damage=random.randint(9, 11),
+    agility=random.randint(7, 9),
+    rarity=1
+)
+
+# strong raids
+bull = Entity(
+    name="Raging bull",
+    hostile=True,
+    description="You better not be waving a red cloth",
+    hp=random.randint(17, 21),
+    damage=random.randint(13, 15),
+    agility=12,
+    rarity=2
+)
+boa = Entity(
+    name="Boa",
+    hostile=True,
+    description="Wait, how did boas get here?!",
+    hp=random.randint(14, 18),
+    damage=random.randint(13, 15),
+    agility=19,
+    rarity=1
+)
+mutant_bee = Entity(
+    name="Mutant bumblebee",
+    hostile=True,
+    description="Do I really smell like honey?",
+    hp=random.randint(11, 17),
+    damage=random.randint(11, 14),
+    agility=21,
+    rarity=1
+)
+# endregion
+# region invasions
+raider = Entity(
+    name="Masked raiders",
+    hostile=True,
+    description="I'm sure they can wear more fashionable clothes",
+    hp=random.randint(7, 11),
+    damage=random.randint(11,14),
+    agility=(9,11),
+    rarity=1
+)
 
 # endregion
